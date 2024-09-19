945083da402ed0cb12694de338398344cf202e6f
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2024-09-19 22:51:31 +0600

    added login page interactive

---
+++ scripts/login.js
@@ -0,0 +1,15 @@
+function getInputs(id) {
+  return document.getElementById(id).value;
+}
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const number = getInputs("number-input");
+    const pin = getInputs("pin-input");
+    if (pin === "1234" && number === "012345678") {
+      window.location.href = "../home.html";
+    } else {
+      alert("failed to login please provide right info");
+    }
+  });
